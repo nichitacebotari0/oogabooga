600c2aca7655a468442c55736983bfaf7ab3e7c1
nichitacebotari0 <nichitacebotari0@gmail.com>
2024-08-24 10:41:44 +0300

    zoom out w/ projection matrix

--- entry_researchwizard.c
+++ entry_researchwizard.c
@@ -12,17 +12,19 @@ int entry(int argc, char **argv)
 	Gfx_Image *player_sprite = load_image_from_disk(fixed_string("../Assets/Player.png"), get_heap_allocator());
 	assert(player_sprite, "Failed to load player sprite");
 
-	Vector2 player_pos = v2(0, 0);
 
+	Vector2 player_pos = v2(0, 0);
+	const float32 second = 1;
+	float64 fpsTime = 0;
+	float64 fps = 0;
 	float64 last_time = os_get_current_time_in_seconds();
 	float64 delta_time = 0;
 	while (!window.should_close)
 	{
 		float64 now = os_get_current_time_in_seconds();
-		if ((int)now != (int)last_time)
-			log("%.2f FPS\n%.2fms", 1.0 / (now - last_time), (now - last_time) * 1000);
 		delta_time = now - last_time;
 		last_time = now;
+
 
 		reset_temporary_storage();
 
@@ -50,22 +52,35 @@ int entry(int argc, char **argv)
 		}
 
 		input_axis = v2_normalize(input_axis);
-		player_pos = v2_add(player_pos, v2_mulf(input_axis, 1 * delta_time));
+		player_pos = v2_add(player_pos, v2_mulf(input_axis, 50 * delta_time));
 
+		draw_frame.projection = m4_make_orthographic_projection(window.width * -0.5, window.width * 0.5, window.height * -0.5, window.height * 0.5, -1, 10);
+		const float64 zoom = 6.0f;
+		draw_frame.view = m4_make_scale(v3(1 / zoom, 1 / zoom, 1));
+
+		Vector2 size = v2(7,11);
 		Matrix4 xform = m4_scalar(1.0);
+		xform = m4_translate(xform, v3(size.x * -0.5, 0, 0));
 		xform = m4_translate(xform, v3(player_pos.x, player_pos.y, 0));
-		draw_image_xform(player_sprite, xform, v2(.5f, .5f), COLOR_GREEN);
+		draw_image_xform(player_sprite, xform, size, COLOR_RED);
 
-		// draw_rect(v2(sin(now), -.8), v2(.5, .25), COLOR_RED);
-
-		// draws line to mouse pos
-		// float aspect = (f32)window.width/(f32)window.height;
-		// float mx = (input_frame.mouse_x/(f32)window.width  * 2.0 - 1.0)*aspect;
-		// float my = input_frame.mouse_y/(f32)window.height * 2.0 - 1.0;
-		// draw_line(v2(-.75, -.75), v2(mx, my), 0.005, COLOR_WHITE);
 
 		os_update();
 		gfx_update();
+		
+		// fps counter
+		if (fpsTime > second)
+		{
+			log("%.2f FPS\n%.2fms", fps, fpsTime);
+			fps = 0;
+			fpsTime -= second;
+		}
+		else
+		{
+			fpsTime += delta_time;
+			fps++;
+
+		}
 	}
 
 	return 0;
